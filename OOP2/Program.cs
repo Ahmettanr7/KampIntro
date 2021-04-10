b794ff0fbbb27bd77c574b88edfcb6607320de7c
Ahmettanr7 <Ahmet Tanrkulu@AHMETTAN>
2021-04-10 13:31:34 +0300

    10 Nisan -KURS-

---
+++ OOP2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RealCustomer customer1 = new RealCustomer();
+            customer1.CustomerId = 1;
+            customer1.CustomerNo = "00001";
+            customer1.TcNo = "444444444444";
+            customer1.CustomerName = "Ahmet";
+            customer1.CustomerSurname = "Tanrıkulu";
+
+            LegalCustomer customer2 = new LegalCustomer();
+            customer2.CustomerId = 2;
+            customer2.CustomerNo = "00002";
+            customer2.TaxNo = "1234567890";
+            customer2.CompanyName = "Tannet";
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(customer1);
+            
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IhtiyacKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            TasitKrediManager tasitKrediManager = new TasitKrediManager();
+            KonutKrediManager konutKrediManager = new KonutKrediManager();
+            EsnafKrediManager esnafKrediManager = new EsnafKrediManager();
+
+            DatabaseLoggerService databaseLoggerService = new DatabaseLoggerService();
+            FileLoggerService fileLoggerService = new FileLoggerService();
+            SmsLoggerService smsLoggerService = new SmsLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService> { databaseLoggerService , smsLoggerService};
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(esnafKrediManager, loggers); 
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasitKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
